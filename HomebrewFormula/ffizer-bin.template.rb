3572063a7e5bf1c77e61c8a9f19535eb8ff2adc2
David Bernard <david.bernard.31@gmail.com>
2021-09-04 18:03:21 +0200

    :bug: fix syntax of template

--- HomebrewFormula/ffizer-bin.template.rb
+++ HomebrewFormula/ffizer-bin.template.rb
@@ -1,15 +1,15 @@
 class FfizerBin < Formula
-  version 'HOMEBREW_VERSION'
+  version "${HOMEBREW_VERSION}"
   desc "ffizer is a files and folders initializer / generator. Create any kind (or part) of project from template."
   homepage "https://github.com/ffizer/ffizer"
   bottle :unneeded
 
   if OS.mac?
-      url "HOMEBREW_ASSET_URL"
-      sha256 "HOMEBREW_SHA256"
+      url "${HOMEBREW_ASSET_URL}"
+      sha256 "${HOMEBREW_SHA256}"
   elsif OS.linux?
-      url "HOMEBREW_ASSET_URL_ALT"
-      sha256 "HOMEBREW_SHA256_ALT"
+      url "${HOMEBREW_ASSET_URL_ALT}"
+      sha256 "${HOMEBREW_SHA256_ALT}"
   end
 
   #conflicts_with "ffizer"
