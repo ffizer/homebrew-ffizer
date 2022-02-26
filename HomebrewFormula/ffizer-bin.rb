1d93a71b7a657e485ef7e3d2de5eb09467687677
David Bernard <david.bernard.31@gmail.com>
2022-02-26 18:51:40 +0100

    :building_construction: use a bash script to generate the formula
    - add support for Apple M1

--- HomebrewFormula/ffizer-bin.rb
+++ HomebrewFormula/ffizer-bin.rb
@@ -1,15 +1,18 @@
 class FfizerBin < Formula
-  version "2.4.1"
+  version "2.4.3"
   desc "ffizer is a files and folders initializer / generator. Create any kind (or part) of project from template."
   homepage "https://github.com/ffizer/ffizer"
-  #bottle :unneeded
+  bottle :unneeded
 
+  if OS.mac? && Hardware::CPU.intel?
+      url "https://github.com/ffizer/ffizer/releases/download/2.4.3/ffizer_2.4.3-x86_64-apple-darwin.tar.gz"
+      sha256 "268ea20e84fcd05e3279e3d95bd4522a8560ed03a62540366cdccf90b3f56d3a  /tmp/shasum_tmp"
   if OS.mac?
-      url "https://github.com/ffizer/ffizer/releases/download/2.4.1/ffizer_2.4.1-x86_64-apple-darwin.tar.gz"
-      sha256 "82702d699a31ee468507e27b0bd8b4aee27e53bd28b19a6713d8ac400eb2e894"
+      url "https://github.com/ffizer/ffizer/releases/download/2.4.3/ffizer_2.4.3-aarch64-apple-darwin.tar.gz"
+      sha256 "25fc6a633db490de735b506199ba60d8808b337cb4d34bd413142d550660d811  /tmp/shasum_tmp"
   elsif OS.linux?
-      url "https://github.com/ffizer/ffizer/releases/download/2.4.1/ffizer_2.4.1-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "589370d0bcce98dcb1164094cf22ef723842f8dc14447fc336f0b89ee029c0a8"
+      url "https://github.com/ffizer/ffizer/releases/download/2.4.3/ffizer_2.4.3-x86_64-unknown-linux-gnu.tar.gz"
+      sha256 "6b7544b1e93ff9c44b92844180d17f15e0be31daff28126a15136ea77f7ea168  /tmp/shasum_tmp"
   end
 
   #conflicts_with "ffizer"
---
+++ make-formula.sh
@@ -0,0 +1,67 @@
+#!/usr/bin/env bash
+set -o errexit
+set -o nounset
+set -o pipefail
+
+GITHUB_REPO="ffizer"
+GITHUB_USER="ffizer"
+
+get_latest_release() {
+    curl --silent "https://api.github.com/repos/$GITHUB_USER/$GITHUB_REPO/releases/latest" | # Get latest release from GitHub api
+        grep '"tag_name":' |                                                                 # Get tag line
+        sed -E 's/.*"([^"]+)".*/\1/'                                                         # Pluck JSON value
+}
+
+shasum_of() {
+    local URL=$1
+    local TMP_FILE=/tmp/shasum_tmp
+    rm -f "$TMP_FILE" || true
+    curl --fail-with-body --silent --output "$TMP_FILE" "$URL"
+    sha256sum "$TMP_FILE"
+    rm -f "$TMP_FILE" || true
+}
+
+VERSION=${1:-$(get_latest_release)}
+HOMEBREW_ASSET_URL_x86_64_apple_darwin="https://github.com/${GITHUB_USER}/${GITHUB_REPO}/releases/download/${VERSION}/${GITHUB_REPO}_${VERSION}-x86_64-apple-darwin.tar.gz"
+HOMEBREW_ASSET_SHA256_x86_64_apple_darwin=$(shasum_of "$HOMEBREW_ASSET_URL_x86_64_apple_darwin")
+HOMEBREW_ASSET_URL_aarch64_apple_darwin="https://github.com/${GITHUB_USER}/${GITHUB_REPO}/releases/download/${VERSION}/${GITHUB_REPO}_${VERSION}-aarch64-apple-darwin.tar.gz"
+HOMEBREW_ASSET_SHA256_aarch64_apple_darwin=$(shasum_of "$HOMEBREW_ASSET_URL_aarch64_apple_darwin")
+HOMEBREW_ASSET_URL_x86_64_unknown_linux_gnu="https://github.com/${GITHUB_USER}/${GITHUB_REPO}/releases/download/${VERSION}/${GITHUB_REPO}_${VERSION}-x86_64-unknown-linux-gnu.tar.gz"
+HOMEBREW_ASSET_SHA256_x86_64_unknown_linux_gnu=$(shasum_of "$HOMEBREW_ASSET_URL_x86_64_unknown_linux_gnu")
+
+cat >HomebrewFormula/ffizer-bin.rb <<EOF
+class FfizerBin < Formula
+  version "${VERSION}"
+  desc "ffizer is a files and folders initializer / generator. Create any kind (or part) of project from template."
+  homepage "https://github.com/${GITHUB_USER}/${GITHUB_REPO}"
+  bottle :unneeded
+
+  if OS.mac? && Hardware::CPU.intel?
+      url "$HOMEBREW_ASSET_URL_x86_64_apple_darwin"
+      sha256 "$HOMEBREW_ASSET_SHA256_x86_64_apple_darwin"
+  if OS.mac?
+      url "$HOMEBREW_ASSET_URL_aarch64_apple_darwin"
+      sha256 "$HOMEBREW_ASSET_SHA256_aarch64_apple_darwin"
+  elsif OS.linux?
+      url "${HOMEBREW_ASSET_URL_x86_64_unknown_linux_gnu}"
+      sha256 "${HOMEBREW_ASSET_SHA256_x86_64_unknown_linux_gnu}"
+  end
+
+  #conflicts_with "${GITHUB_REPO}"
+
+  def install
+    bin.install "${GITHUB_REPO}"
+    # man1.install "doc/${GITHUB_REPO}.1"
+
+    # bash_completion.install "complete/${GITHUB_REPO}.bash"
+    # fish_completion.install "complete/${GITHUB_REPO}.fish"
+    # zsh_completion.install "complete/_${GITHUB_REPO}"
+  end
+
+  test do
+    system "#{bin}/${GITHUB_REPO}", "--help"
+  end
+end
+EOF
+
+echo "::set-output name=version::${VERSION}"
