0ac0475a41c3619d50260abd8d89c6e57d23f141
davidB <davidB@users.noreply.github.com>
2022-02-27 15:14:08 +0000

    :arrow_up: update formula for ffizer@2.4.4

--- HomebrewFormula/ffizer-bin.rb
+++ HomebrewFormula/ffizer-bin.rb
@@ -1,21 +1,28 @@
+# frozen_string_literal: true
+
+#
+# Homebrew formula for ffizer-bin
+#
+# directly download the pre-built binary from ffizer github repository
 class FfizerBin < Formula
-  version "2.4.3"
-  desc "ffizer is a files and folders initializer / generator. Create any kind (or part) of project from template."
+  desc "Generate or update files, folders (and project) from templates with ffizer"
   homepage "https://github.com/ffizer/ffizer"
-  #bottle :unneeded
+  version "2.4.4"
+  license "CC0-1.0"
+  # bottle :unneeded
 
   if OS.mac? && Hardware::CPU.intel?
-      url "https://github.com/ffizer/ffizer/releases/download/2.4.3/ffizer_2.4.3-x86_64-apple-darwin.tar.gz"
-      sha256 "9e586cac24d63e32a80b3f56d38602ddd7f210fd3a5a81b20e1948f5b8c3cd05"
+    url "https://github.com/ffizer/ffizer/releases/download/2.4.4/ffizer_2.4.4-x86_64-apple-darwin.tar.gz"
+    sha256 ""
   elsif OS.mac?
-      url "https://github.com/ffizer/ffizer/releases/download/2.4.3/ffizer_2.4.3-aarch64-apple-darwin.tar.gz"
-      sha256 "32a4fa453887685a9ad3fca382609937003106428c46304c69794f54afd74ad9"
+    url "https://github.com/ffizer/ffizer/releases/download/2.4.4/ffizer_2.4.4-aarch64-apple-darwin.tar.gz"
+    sha256 ""
   elsif OS.linux?
-      url "https://github.com/ffizer/ffizer/releases/download/2.4.3/ffizer_2.4.3-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "c1baadf1bedd2f9883ea1fa23866364231dd8cc57ea60ecd51a1040a6f4a6915"
+    url "https://github.com/ffizer/ffizer/releases/download/2.4.4/ffizer_2.4.4-x86_64-unknown-linux-gnu.tar.gz"
+    sha256 ""
   end
 
-  #conflicts_with "ffizer"
+  # conflicts_with "ffizer"
 
   def install
     bin.install "ffizer"
