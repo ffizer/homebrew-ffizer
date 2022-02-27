75744cee8fee767b7539112722cbd4a777569764
davidB <davidB@users.noreply.github.com>
2022-02-27 15:24:11 +0000

    :arrow_up: update formula for ffizer@2.4.4

--- HomebrewFormula/ffizer-bin.rb
+++ HomebrewFormula/ffizer-bin.rb
@@ -13,13 +13,13 @@ class FfizerBin < Formula
 
   if OS.mac? && Hardware::CPU.intel?
     url "https://github.com/ffizer/ffizer/releases/download/2.4.4/ffizer_2.4.4-x86_64-apple-darwin.tar.gz"
-    sha256 ""
+    sha256 "250cf118371f4b7d7559b40aa622d060c7e156ac59d89202790a1d52f31db7f0"
   elsif OS.mac?
     url "https://github.com/ffizer/ffizer/releases/download/2.4.4/ffizer_2.4.4-aarch64-apple-darwin.tar.gz"
-    sha256 ""
+    sha256 "e079ed4a83f1e09b75a742b903f00b99a5b3989a1c6935b8b031ceb38f70c4be"
   elsif OS.linux?
     url "https://github.com/ffizer/ffizer/releases/download/2.4.4/ffizer_2.4.4-x86_64-unknown-linux-gnu.tar.gz"
-    sha256 ""
+    sha256 "96799ace73edb16a056503c1bcb056ba616477b809630e41af67107f798043e6"
   end
 
   # conflicts_with "ffizer"
