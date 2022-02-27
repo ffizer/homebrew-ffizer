d5b7ab2ce8df55399a81daf3b5e725587ca264db
David Bernard <david.bernard.31@gmail.com>
2022-02-27 16:12:35 +0100

    :rotating_light: fix indentation and suggestion reporting by `brew audit`

--- make-formula.sh
+++ make-formula.sh
@@ -7,18 +7,18 @@ GITHUB_REPO="ffizer"
 GITHUB_USER="ffizer"
 
 get_latest_release() {
-    curl --silent "https://api.github.com/repos/$GITHUB_USER/$GITHUB_REPO/releases/latest" | # Get latest release from GitHub api
-        grep '"tag_name":' |                                                                 # Get tag line
-        sed -E 's/.*"([^"]+)".*/\1/'                                                         # Pluck JSON value
+  curl --silent "https://api.github.com/repos/$GITHUB_USER/$GITHUB_REPO/releases/latest" | # Get latest release from GitHub api
+    grep '"tag_name":' |                                                                   # Get tag line
+    sed -E 's/.*"([^"]+)".*/\1/'                                                           # Pluck JSON value
 }
 
 shasum_of() {
-    local URL=$1
-    local TMP_FILE=/tmp/shasum_tmp
-    rm -f "$TMP_FILE" || true
-    curl -SL --fail-with-body --output "$TMP_FILE" "$URL"
-    sha256sum "$TMP_FILE" | cut -d' ' -f1
-    rm -f "$TMP_FILE" || true
+  local URL=$1
+  local TMP_FILE=/tmp/shasum_tmp
+  rm -f "$TMP_FILE" || true
+  curl -SL --fail-with-body --output "$TMP_FILE" "$URL"
+  sha256sum "$TMP_FILE" | cut -d' ' -f1
+  rm -f "$TMP_FILE" || true
 }
 
 VERSION=${1:-$(get_latest_release)}
@@ -30,24 +30,31 @@ HOMEBREW_ASSET_URL_x86_64_unknown_linux_gnu="https://github.com/${GITHUB_USER}/$
 HOMEBREW_ASSET_SHA256_x86_64_unknown_linux_gnu=$(shasum_of "$HOMEBREW_ASSET_URL_x86_64_unknown_linux_gnu")
 
 cat >HomebrewFormula/ffizer-bin.rb <<EOF
+# frozen_string_literal: true
+
+#
+# Homebrew formula for ffizer-bin
+#
+# directly download the pre-built binary from ffizer github repository
 class FfizerBin < Formula
-  version "${VERSION}"
-  desc "ffizer is a files and folders initializer / generator. Create any kind (or part) of project from template."
+  desc "Generate or update files, folders (and project) from templates with ffizer"
   homepage "https://github.com/${GITHUB_USER}/${GITHUB_REPO}"
-  #bottle :unneeded
+  version "${VERSION}"
+  license "CC0-1.0"
+  # bottle :unneeded
 
   if OS.mac? && Hardware::CPU.intel?
-      url "$HOMEBREW_ASSET_URL_x86_64_apple_darwin"
-      sha256 "$HOMEBREW_ASSET_SHA256_x86_64_apple_darwin"
+    url "$HOMEBREW_ASSET_URL_x86_64_apple_darwin"
+    sha256 "$HOMEBREW_ASSET_SHA256_x86_64_apple_darwin"
   elsif OS.mac?
-      url "$HOMEBREW_ASSET_URL_aarch64_apple_darwin"
-      sha256 "$HOMEBREW_ASSET_SHA256_aarch64_apple_darwin"
+    url "$HOMEBREW_ASSET_URL_aarch64_apple_darwin"
+    sha256 "$HOMEBREW_ASSET_SHA256_aarch64_apple_darwin"
   elsif OS.linux?
-      url "${HOMEBREW_ASSET_URL_x86_64_unknown_linux_gnu}"
-      sha256 "${HOMEBREW_ASSET_SHA256_x86_64_unknown_linux_gnu}"
+    url "${HOMEBREW_ASSET_URL_x86_64_unknown_linux_gnu}"
+    sha256 "${HOMEBREW_ASSET_SHA256_x86_64_unknown_linux_gnu}"
   end
 
-  #conflicts_with "${GITHUB_REPO}"
+  # conflicts_with "${GITHUB_REPO}"
 
   def install
     bin.install "${GITHUB_REPO}"
