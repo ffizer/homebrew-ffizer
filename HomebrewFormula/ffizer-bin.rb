b479a635898cf0b8877bf3f7526d68b1fd78ccbe
davidB <davidB@users.noreply.github.com>
2022-11-10 14:00:02 +0000

    :arrow_up: update formula for ffizer@2.6.1

--- HomebrewFormula/ffizer-bin.rb
+++ HomebrewFormula/ffizer-bin.rb
@@ -7,19 +7,19 @@
 class FfizerBin < Formula
   desc "Generate or update files, folders (and project) from templates with ffizer"
   homepage "https://github.com/ffizer/ffizer"
-  version "2.5.1"
+  version "2.6.1"
   license "CC0-1.0"
   # bottle :unneeded
 
   if OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/ffizer/ffizer/releases/download/2.5.1/ffizer_2.5.1-x86_64-apple-darwin.tar.gz"
-    sha256 "e571fc07e4c22bd15b7c95504d5a5f50f4541b42cfa9a33c0b5ad2a422b0861b"
+    url "https://github.com/ffizer/ffizer/releases/download/2.6.1/ffizer_2.6.1-x86_64-apple-darwin.tar.gz"
+    sha256 "ba48f4f54ed8071c505cb2ce8c212be94a0d5d0ac3d52e49ae25622172f237f5"
   elsif OS.mac?
-    url "https://github.com/ffizer/ffizer/releases/download/2.5.1/ffizer_2.5.1-aarch64-apple-darwin.tar.gz"
-    sha256 "bba7d3695f494934e8030623bec6de6568dd3a567c8bd037561b660a770a5f2f"
+    url "https://github.com/ffizer/ffizer/releases/download/2.6.1/ffizer_2.6.1-aarch64-apple-darwin.tar.gz"
+    sha256 "956d5fddb517c729b302d1ed28de6cab4da630ec276ef97d973463725f14e41c"
   elsif OS.linux?
-    url "https://github.com/ffizer/ffizer/releases/download/2.5.1/ffizer_2.5.1-x86_64-unknown-linux-gnu.tar.gz"
-    sha256 "066075368233c0c004ac48c57df807cbe7b98471babbd4d62f49d4195e090c05"
+    url "https://github.com/ffizer/ffizer/releases/download/2.6.1/ffizer_2.6.1-x86_64-unknown-linux-gnu.tar.gz"
+    sha256 "4c2938806873d5b26577db6055dc16b232c145a706315e2901837f2b27b81aa8"
   end
 
   # conflicts_with "ffizer"
