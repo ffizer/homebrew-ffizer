1a414b59c58c51fd417427f62bc9cefc6c67c1c9
davidB <davidB@users.noreply.github.com>
2023-04-10 16:52:44 +0000

    :arrow_up: update formula for ffizer@2.9.0

--- HomebrewFormula/ffizer-bin.rb
+++ HomebrewFormula/ffizer-bin.rb
@@ -7,19 +7,22 @@
 class FfizerBin < Formula
   desc "Generate or update files, folders (and project) from templates with ffizer"
   homepage "https://github.com/ffizer/ffizer"
-  version "2.8.0"
+  version "2.9.0"
   license "CC0-1.0"
   # bottle :unneeded
 
   if OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/ffizer/ffizer/releases/download/2.8.0/ffizer_2.8.0-x86_64-apple-darwin.tgz"
-    sha256 "9697293226d571e961818e8fb0263e9d120e7409628734db71bf4e5d7693512e"
+    url "https://github.com/ffizer/ffizer/releases/download/2.9.0/ffizer_2.9.0-x86_64-apple-darwin.tgz"
+    sha256 "1e88c4832501f351f153f8aff3d178fb4788fb6a070fe0037bfd2674843d708d"
   elsif OS.mac?
-    url "https://github.com/ffizer/ffizer/releases/download/2.8.0/ffizer_2.8.0-aarch64-apple-darwin.tgz"
-    sha256 "a22b24040b7647d2a04b128f3b84f822ec6165a867030db72287591705a3c0ce"
+    url "https://github.com/ffizer/ffizer/releases/download/2.9.0/ffizer_2.9.0-aarch64-apple-darwin.tgz"
+    sha256 "31036539b2943efcb4055d28d33680846d9470bfd9458aa4a3459e5ebda172d9"
+  elsif OS.linux? && Hardware::CPU.intel?
+    url "https://github.com/ffizer/ffizer/releases/download/2.9.0/ffizer_2.9.0-x86_64-unknown-linux-musl.tgz"
+    sha256 ""
   elsif OS.linux?
-    url "https://github.com/ffizer/ffizer/releases/download/2.8.0/ffizer_2.8.0-x86_64-unknown-linux-gnu.tgz"
-    sha256 "a2161a89b304fc44f4fb16d214117f04e7d888f6dce81b39ace05e4e3a5e846b"
+    url "https://github.com/ffizer/ffizer/releases/download/2.9.0/ffizer_2.9.0-aarch64-unknown-linux-musl.tgz"
+    sha256 ""
   end
 
   # conflicts_with "ffizer"
