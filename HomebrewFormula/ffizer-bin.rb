f17912c15499fd7a6e62f182765d09b9a11e5691
davidB <davidB@users.noreply.github.com>
2023-04-10 18:23:29 +0000

    :arrow_up: update formula for ffizer@2.10.2

--- HomebrewFormula/ffizer-bin.rb
+++ HomebrewFormula/ffizer-bin.rb
@@ -18,8 +18,8 @@ class FfizerBin < Formula
     url "https://github.com/ffizer/ffizer/releases/download/2.10.2/ffizer_2.10.2-aarch64-apple-darwin.tgz"
     sha256 "7d6bc2b0263f9aea40ff5571ee9496648cc2f9c41fd7a290b963f248da5762c3"
   elsif OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/ffizer/ffizer/releases/download/2.10.2/ffizer_2.10.2-x86_64-unknown-linux-musl.tgz"
-    sha256 "8d4d88c514db3833ad5934af2c0f4aa3d6552168c16028cd187f4240844ddd9c"
+    url "https://github.com/ffizer/ffizer/releases/download/2.10.2/ffizer_2.10.2-x86_64-unknown-linux-gnu.tgz"
+    sha256 ""
   elsif OS.linux?
     url "https://github.com/ffizer/ffizer/releases/download/2.10.2/ffizer_2.10.2-aarch64-unknown-linux-musl.tgz"
     sha256 "1d37f1e7b07f25f222e01d3068855cb5b0f1f124d2bbab76bd0599ddf1e80bb5"
