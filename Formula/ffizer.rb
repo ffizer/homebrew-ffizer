c3b01cf95041fcca55b286ac9f19db7436411186
David Bernard <davidB@users.noreply.github.com>
2023-12-10 21:29:15 +0100

    hot fix: Update ffizer.rb

--- Formula/ffizer.rb
+++ Formula/ffizer.rb
@@ -10,7 +10,7 @@ class Ffizer < Formula
     sha256 "0a90ce200ddc223f28a0b51f00d4cc87af01662d730a044e0e37eca599fd9957"
 
     def install
-      bin.install "ffizer_2.12.0-aarch64-unknown-linux-gnu" => "ffizer"
+      bin.install "ffizer_2.12.0-aarch64-unknown-linux-gnu/ffizer" => "ffizer"
     end
   end
   if OS.linux? && Hardware::CPU.intel?
@@ -18,7 +18,7 @@ class Ffizer < Formula
     sha256 "ee9b74bf94d7c248da73a35f6124d8ef4d0f7810c505534ad1fae5e921755e06"
 
     def install
-      bin.install "ffizer_2.12.0-x86_64-unknown-linux-gnu" => "ffizer"
+      bin.install "ffizer_2.12.0-x86_64-unknown-linux-gnu/ffizer" => "ffizer"
     end
   end
   if OS.mac? && Hardware::CPU.intel?
@@ -26,7 +26,7 @@ class Ffizer < Formula
     sha256 "e9e296ba8456548f36731f61c1d2ab3f01db85fb6bd8761a4067c39e7d97ccfe"
 
     def install
-      bin.install "ffizer_2.12.0-all-apple-darwin" => "ffizer"
+      bin.install "ffizer_2.12.0-all-apple-darwin/ffizer" => "ffizer"
     end
   end
   if OS.mac? && Hardware::CPU.arm?
@@ -34,7 +34,7 @@ class Ffizer < Formula
     sha256 "6bfc7b7018ade778c4f2f996ab218920023aa04e10439b02bd2b0be96eacaa42"
 
     def install
-      bin.install "ffizer_2.12.0-aarch64-apple-darwin" => "ffizer"
+      bin.install "ffizer_2.12.0-aarch64-apple-darwin/ffizer" => "ffizer"
     end
   end
   if OS.mac? && Hardware::CPU.intel?
@@ -42,13 +42,13 @@ class Ffizer < Formula
     sha256 "f592753e595ad2e2b14b50d904fb90a3e8f8af1077ea882f5e64d6f312df6fb1"
 
     def install
-      bin.install "ffizer_2.12.0-x86_64-apple-darwin" => "ffizer"
+      bin.install "ffizer_2.12.0-x86_64-apple-darwin/ffizer" => "ffizer"
     end
   end
 
 
   test do
     output = shell_output("#{bin}/ffizer --version")
-    assert_match "2.12.0", output
+    assert_match "ffizer 2.12.0", output
   end
 end
