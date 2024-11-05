93dd275b69e9d2a490244c32a8f9d103989dce34
davidB <10675+davidB@users.noreply.github.com>
2024-11-05 19:53:26 +0000

    :arrow_up: update formula for ffizer@2.13.0

--- HomebrewFormula/ffizer-bin.rb
+++ HomebrewFormula/ffizer-bin.rb
@@ -7,22 +7,22 @@
 class FfizerBin < Formula
   desc "Generate or update files, folders (and project) from templates with ffizer"
   homepage "https://github.com/ffizer/ffizer"
-  version "2.10.3"
+  version "2.13.0"
   license "CC0-1.0"
   # bottle :unneeded
 
   if OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/ffizer/ffizer/releases/download/2.10.3/ffizer_2.10.3-x86_64-apple-darwin.tgz"
-    sha256 "b88f89053ab6954ca5b04a32df889871bf2ba3c6db8619ea9e9ccf8bde970de7"
+    url "https://github.com/ffizer/ffizer/releases/download/2.13.0/ffizer_2.13.0-x86_64-apple-darwin.tgz"
+    sha256 ""
   elsif OS.mac?
-    url "https://github.com/ffizer/ffizer/releases/download/2.10.3/ffizer_2.10.3-aarch64-apple-darwin.tgz"
-    sha256 "a04897869f1682b57d5c814d81e5d5be35e1f09c06c8ad55cd5084ad6da17c49"
+    url "https://github.com/ffizer/ffizer/releases/download/2.13.0/ffizer_2.13.0-aarch64-apple-darwin.tgz"
+    sha256 ""
   elsif OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/ffizer/ffizer/releases/download/2.10.3/ffizer_2.10.3-x86_64-unknown-linux-gnu.tgz"
-    sha256 "5b54f001b18eba8b7448b4e5b4a9c27fb0fca3e94617cae348cb11411949d9d6"
+    url "https://github.com/ffizer/ffizer/releases/download/2.13.0/ffizer_2.13.0-x86_64-unknown-linux-gnu.tgz"
+    sha256 ""
   elsif OS.linux?
-    url "https://github.com/ffizer/ffizer/releases/download/2.10.3/ffizer_2.10.3-aarch64-unknown-linux-musl.tgz"
-    sha256 "d72d00676fabc04b17e07d758181bad23e9d4b4621baa124a5717b9d6969d4d6"
+    url "https://github.com/ffizer/ffizer/releases/download/2.13.0/ffizer_2.13.0-aarch64-unknown-linux-musl.tgz"
+    sha256 ""
   end
 
   # conflicts_with "ffizer"
