e3340b3ebe688f335010a9b2811b3616fc28c071
jreleaserbot <jreleaser@kordamp.org>
2026-07-14 12:36:41 +0000

    ffizer 2.13.12

--- Formula/ffizer.rb
+++ Formula/ffizer.rb
@@ -1,26 +1,26 @@
-# Generated with JReleaser 1.24.0 at 2026-06-09T10:14:58.82039339Z
+# Generated with JReleaser 1.25.0 at 2026-07-14T12:36:25.688207294Z
 
 class Ffizer < Formula
   desc "ffizer is a files and folders initializer / generator."
   homepage "https://ffizer.github.io/ffizer/book/"
-  version "2.13.11"
+  version "2.13.12"
   license "CC0-1.0"
 
   if OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-    url "https://github.com/ffizer/ffizer/releases/download/2.13.11/ffizer_2.13.11-aarch64-unknown-linux-gnu.zip"
-    sha256 "cf24464adc978c343e46b98381fd824f43893db0690c12f7093486441db19682"
+    url "https://github.com/ffizer/ffizer/releases/download/2.13.12/ffizer_2.13.12-aarch64-unknown-linux-gnu.zip"
+    sha256 "89dc64568cf9b30fae0683409d55975505f4d6ab29298702d022b20685e33e03"
   end
   if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/ffizer/ffizer/releases/download/2.13.11/ffizer_2.13.11-x86_64-unknown-linux-gnu.zip"
-    sha256 "4750bc265be9e3e687e4938bb9e7d4dae0d22d1a58258ed7c5719df3cb47bd49"
+    url "https://github.com/ffizer/ffizer/releases/download/2.13.12/ffizer_2.13.12-x86_64-unknown-linux-gnu.zip"
+    sha256 "20044ccba9088c79046e89a3928eacfb9ccef5367538f6b6daa609b8a9761919"
   end
   if OS.mac? && Hardware::CPU.arm?
-    url "https://github.com/ffizer/ffizer/releases/download/2.13.11/ffizer_2.13.11-aarch64-apple-darwin.zip"
-    sha256 "175c73f9f0ce9b7a6905a98634353da406b3196cadbb0adee3619a1c6ac7e104"
+    url "https://github.com/ffizer/ffizer/releases/download/2.13.12/ffizer_2.13.12-aarch64-apple-darwin.zip"
+    sha256 "335b53bef127c7d323ec74176f0bda31b0fd886db6f1022c616a99b3a1b72530"
   end
   if OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/ffizer/ffizer/releases/download/2.13.11/ffizer_2.13.11-x86_64-apple-darwin.zip"
-    sha256 "d354cd4bdbfc11c2b0840af370dcd23be7936cd6c4584bf525e5304b0ffde245"
+    url "https://github.com/ffizer/ffizer/releases/download/2.13.12/ffizer_2.13.12-x86_64-apple-darwin.zip"
+    sha256 "d2ccd23c41cce7fb122e25a5783bd45d1cb73d9b1cd105c860566ca6a377555d"
   end
 
 
@@ -31,6 +31,6 @@ class Ffizer < Formula
 
   test do
     output = shell_output("#{bin}/ffizer --version")
-    assert_match "2.13.11", output
+    assert_match "2.13.12", output
   end
 end
